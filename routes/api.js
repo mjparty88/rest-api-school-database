547bda3baf6c6195b1660d3896c9117c02cf5016
Matt Partridge <partridgem88@gmail.com>
2020-08-11 21:05:19 +1000

    - Refactored validations and authenticaiton middleware into seperate files within validation folder

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const apiRouter = express.Router()
-const { check, body, validationResult } = require('express-validator');
 const User = require('../models').User
 const Course = require('../models').Course
 const bcryptjs = require('bcryptjs');
 const auth = require('basic-auth');
+const { validationResult } = require('express-validator');
+const {userValidationChain} = require('../validation/userValidation');
+const {courseValidationChain} = require('../validation/courseValidation');
+const {authenticationFunc} = require('../validation/authentication');
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -21,68 +24,7 @@ function asyncHandler(cb){
   }
 }
 
-//middleware validation for the user resources (using express-validator)
-const userValidationChain = [
-  check('firstName')
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide a first name for the user using the key firstName")
-    .isAlpha().withMessage("firstName must be a string, with only alpha characters"),
-  check('lastName')
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide a last name for the user using the key lastName")
-    .isAlpha().withMessage("lastName must be a string, with only alpha characters"),
-  check('emailAddress')
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide an email address for the user using the key emailAddress")
-    .isEmail().withMessage("You must provide a valid email address"),
-  check('password')
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide a password for the user using the key password")
-  ];
 
-//middleware validation for the course resources (using express-validator)
-const courseValidationChain = [
-  check('title')
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide a title for the course with the key title")
-    .isString().withMessage("The title must be a string"), //isAlpha doesn't work here because spaces should be allowed
-  check('description')
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide a description for the course with the key description")
-    .isString().withMessage("The description must be a string (it is a text field)"), //isAlpha doesn't work here because spaces should be allowed
-  body('estimatedTime')
-    .if(body('estimatedTime').exists()) //only validate the estimatedTime field if its been included in the request body
-    .isString().withMessage("If you provide information for estimatedTime, it should be in string format"),
-  body('materialsNeeded')
-    .if(body('materialsNeeded').exists()) //only validate the materialsNeeded field if its been included in the request body
-    .isString().withMessage("If you provide information about the materialsNeeded, it should be in string format"),
-  check('userId') //note, I don't need to explicitly check the foreignKey constraint, because the Sequelize model captures it.
-    .exists({checkNull: true, checkFalsy: true}).withMessage("You must provide the id of the user this course belongs to")
-    .isInt({ allow_leading_zeroes: false }).withMessage("You must provide a valid userId")
-];
-
-//authentication middleware
-const authenticationFunc = async function authenticateUser (req, res, next) {
-  const credentials = auth(req); // Parse the user's credentials from the Authorization header.
-  if (credentials) { // If the user's credentials are available...
-    const users = await User.findAll({attributes: ['id','emailAddress', 'password']});
-    const matchedUser = users.find(user => user.emailAddress === credentials.name); // Attempt to retrieve the user from the data store by their username (i.e. the user's "key" from the Authorization header).
-    if (matchedUser) { // If a user was successfully retrieved from the data store...
-      const authenticated = bcryptjs.compareSync(credentials.pass, matchedUser.password); //use bycrypt to compare the password from the auth header, against the matched users' password
-      if (authenticated) { // If the passwords match...
-        console.log(`Authentication successful for username: ${matchedUser.emailAddress}`);
-        req.currentUser = matchedUser; //add the user's details to the request object
-        next();
-      } else {
-        res.status(403).json({
-          message: "Forbidden: The password did not match the user credential."
-        })
-      }
-    } else {
-      res.status(403).json({
-        message: "Forbidden: No user matches the credential provided."
-      })
-    }
-  } else {
-    res.status(401).json({
-      message: "Unauthorized: No credentials provided in the WWW-authenticate header."
-    })
-  }
-}
 
 
 //API routes will be held here
@@ -171,7 +113,7 @@ apiRouter.post('/courses', authenticationFunc, courseValidationChain, asyncHandl
         description: req.body.description,
         estimatedTime: req.body.estimatedTime,
         materialsNeeded: req.body.materialsNeeded,
-        userId: req.body.userId
+        userId: req.currentUser.id //the authenticated user creating the course is the only person allowed to edit it
       })
       res.status(201).location(`/api/courses/${course.id}`).end()
     } catch(error) {
